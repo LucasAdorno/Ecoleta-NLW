180a973e39d94aff01047ffa59e10ca5f083e419
LucasAdorno <lucasadorno2702@gmail.com>
2020-06-03 00:57:43 -0300

    Otimização dos selects

---
+++ scripts/create-point.js
@@ -0,0 +1,40 @@
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
+
+function populateUFs(){
+    const ufSelect = document.querySelector("select[name=uf]")
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome")
+    .then( res => res.json() )
+    .then( states => {
+        for(state of states){
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+        }
+
+    } )
+}
+
+populateUFs();
+
+function getCities(event){ 
+    const eventValue =event.target.value;  
+    const citySelect = document.querySelector("select[name=city]");
+    const stateInput = document.querySelector("select[name=state]");
+    citySelect.innerHTML = `<option value="">Selecione a cidade</option>`;
+    const ufValue = event.target.value
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios `
+
+    fetch(url)
+    .then( res => res.json() )
+    .then( cities => {
+        for(city of cities){
+            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+        }
+        if (eventValue == 1234) {
+            citySelect.setAttribute('disabled', 'disabled')
+        }
+        else{
+         citySelect.disabled = false;}
+    } )
+
+}
