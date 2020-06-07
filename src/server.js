8386bdc296650001c6d9bfae5cdd3c8dabd92558
LucasAdorno <lucasadorno2702@gmail.com>
2020-06-07 07:35:41 -0300

    end of day 4

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const server = express()
+
+server.use(express.static("public"))
+
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache:true
+} )
+
+
+server.get("/",(req,res) => {
+   return res.render("index.html", {title: "Um titulo"})
+} )
+
+server.get("/create-point",(req,res) => {
+   return res.render("create-point.html")
+} )
+
+server.get("/search",(req,res) => {
+    return res.render("search-results.html")
+ } )
+
+
+server.listen(3000)
+
